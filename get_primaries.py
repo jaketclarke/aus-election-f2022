7adac58dcecad386c1c6d0a373530a46a2181813
Jake Clarke <Jake.Clarke@secnewgate.com.au>
2025-05-04 13:39:23 +1000

    fix to use env var

--- get_primaries.py
+++ get_primaries.py
@@ -29,7 +29,7 @@ create_directory_if_not_exists(WORKING_DIRECTORY)
 # get primary results
 STATES_TERRITORIES = ["NSW", "VIC", "QLD", "WA", "SA", "TAS", "ACT", "NT"]
 for st in STATES_TERRITORIES:
-    url = f"https://results.aec.gov.au/27966/Website/Downloads/HouseStateFirstPrefsByPollingPlaceDownload-27966-{st}.csv"
+    url = f"https://tallyroom.aec.gov.au/Downloads/HouseStateFirstPrefsByPollingPlaceDownload-{ELECTION_CODE}-{st}.csv"
     get_file_from_url(url=url, output_directory=INPUT_DIRECTORY)
 
 
