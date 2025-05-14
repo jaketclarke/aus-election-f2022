f40e17ad481a1ba31c6dd71c3b48d2257a53daa5
Jake Clarke <Jake.Clarke@secnewgate.com.au>
2025-05-14 15:05:41 +1000

    tidy up get primaries

--- get_primaries.py
+++ get_primaries.py
@@ -1,3 +1,4 @@
+"""code to get AEC data for an election code"""
 import os
 import logging
 from dotenv import load_dotenv
@@ -18,6 +19,7 @@ load_dotenv()
 logging.basicConfig()
 DEBUG_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
 ELECTION_CODE = ensure_env_var_exists("ELECTION_CODE")
+logging.warning("running for election %s", ELECTION_CODE)
 
 print(f"debug level: {DEBUG_LEVEL}, running for election {ELECTION_CODE}")
 logging.basicConfig(level=DEBUG_LEVEL.upper())
@@ -29,8 +31,8 @@ create_directory_if_not_exists(WORKING_DIRECTORY)
 # get primary results
 STATES_TERRITORIES = ["NSW", "VIC", "QLD", "WA", "SA", "TAS", "ACT", "NT"]
 for st in STATES_TERRITORIES:
-    url = f"https://tallyroom.aec.gov.au/Downloads/HouseStateFirstPrefsByPollingPlaceDownload-{ELECTION_CODE}-{st}.csv"
-    get_file_from_url(url=url, output_directory=INPUT_DIRECTORY)
+    URL = f"https://tallyroom.aec.gov.au/Downloads/HouseStateFirstPrefsByPollingPlaceDownload-{ELECTION_CODE}-{st}.csv"
+    get_file_from_url(url=URL, output_directory=INPUT_DIRECTORY)
 
 
 merged_data = merge_csv_files(INPUT_DIRECTORY, "HouseStateFirstPrefs")
